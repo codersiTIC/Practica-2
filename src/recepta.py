46a05783218ab648aa0d5fccc3a0ed6bc2d183a7
Anass <anassanhari@gmail.com>
2020-03-08 12:46:31 +0100

    He corregit tots els doctest, pels canvis que va dir el Sebastia, recepta, receptari casi documentat, falta parametres sphinx i ho tenim

--- src/recepta.py
+++ src/recepta.py
@@ -26,11 +26,13 @@ class Recepta(object):
             :type p: str
             :returns:
 
-        >>> r = Recepta('Pastís Xocolata', [('Xocolata', 200), ('Farina', 500)])
-        >>> r.afegeix_ingredient('LLet', 150)
-        [('Xocolata', 200), ('Farina', 500), ('Llet', 150)]
-        >>> r.afegeix_ingredient('llet', 100)
-        [('Xocolata', 200), ('Farina', 500), ('Llet', 250)]
+        >>> r = Recepta('Pastís Xocolata')
+        >>> r.afegeix_ingredient('Llet', 150)
+        [('Llet', 150)]
+        >>> r.afegeix_ingredient('xocolata', 200)
+        [('Llet', 150), ('xocolata', 200)]
+        >>> r.afegeix_ingredient('xocolata', 200)
+        [('Llet', 150), ('xocolata', 400)]
         '''
         i = 0
         for element in self._ingredients:
@@ -46,18 +48,21 @@ class Recepta(object):
             self._ingredients.append((Producte(p).producte,q))
 
         return self._ingredients
-
-
+        
     def conte_ingredient(self,p):
         '''
         Donat un producte, retorna True si està dins la recepta
 
-        >>> llimonada = Recepta('llimonada', [('lLimona', 50), ('aigua', 1000), ('sucre', 10)])
-        >>> llimonada.conte_ingredient('Llimona')
+        >>> suc = Recepta('suc')
+        >>> suc.afegeix_ingredient('taronja', 200)
+        [('taronja', 200)]
+        >>> suc.afegeix_ingredient('aigua', 1000)
+        [('taronja', 200), ('aigua', 1000)]
+        >>> suc.conte_ingredient('taronja')
         True
-        >>> llimonada.conte_ingredient('Aigua')
+        >>> suc.conte_ingredient('aigua')
         True
-        >>> llimonada.conte_ingredient('PoMa')
+        >>> suc.conte_ingredient('poma')
         False
         '''
 
@@ -71,14 +76,17 @@ class Recepta(object):
         '''
         -- Retorna la quantitat de producte p en grams o 0 si no en conté --
 
-        >>> r = Recepta('Banana split', [('Banana', 200),('Xocolata', 100)])
+        >>> r = Recepta('Banana split')
+        >>> r.afegeix_ingredient('Banana', 200)
+        [('Banana', 200)]
+        >>> r.afegeix_ingredient('Xocolata', 100)
+        [('Banana', 200), ('Xocolata', 100)]
         >>> r.quantitat_ingredient('Banana')
         200
         >>> r.quantitat_ingredient('Blueberry')
         0
         >>> r.quantitat_ingredient('Xocolata')
         100
-
         '''
         for element in self._ingredients:
             if element[0] == p:
@@ -92,9 +100,14 @@ class Recepta(object):
         '''
         -- Retorna el pes base total de la recepta en grams --
 
-        >>> llimonada = Recepta('llimonada', [('llimona', 50), ('aigua', 1000), ('sucre', 10)])
+
+        >>> llimonada = Recepta('llimonada')
+        >>> llimonada.afegeix_ingredient('Llimona', 50)
+        [('Llimona', 50)]
+        >>> llimonada.afegeix_ingredient('Aigua', 1000)
+        [('Llimona', 50), ('Aigua', 1000)]
         >>> llimonada.pes_total()
-        1060
+        1050
         '''
 
         return sum([tupla[1] for tupla in self._ingredients])
@@ -104,10 +117,14 @@ class Recepta(object):
         '''
         -- Retorna la llista d'ingredients --
 
-        >>> llimonada = Recepta('llimonada', [('Llimona', 50), ('Aigua', 1000)])
+        >>> llimonada = Recepta('llimonada')
+        >>> llimonada.afegeix_ingredient('Llimona', 50)
+        [('Llimona', 50)]
+        >>> llimonada.afegeix_ingredient('Aigua', 1000)
+        [('Llimona', 50), ('Aigua', 1000)]
         >>> llimonada.ingredients()
         ['Llimona', 'Aigua']
-        >>> llimonada.afegeix_ingredient('sucre', 50)
+        >>> llimonada.afegeix_ingredient('Sucre', 50)
         [('Llimona', 50), ('Aigua', 1000), ('Sucre', 50)]
         >>> llimonada.ingredients()
         ['Llimona', 'Aigua', 'Sucre']
--- src/receptari.py
+++ src/receptari.py
@@ -21,20 +21,35 @@ class Receptari(object):
         self._receptes = dict()
         self._productes = dict()
 
+
     def afegeix_recepta(self,n):
         '''
         Modificador. Afegeix una recepta de nom n al receptari. Si existia una recepta del mateix
         nom es queixa.
         La recepta, inicialment no té ingredients.
+
+        >>> Receptari = Receptari()
+        >>> Receptari.afegeix_recepta('Pastís Xocolata')
+        >>> Receptari.afegeix_recepta('Pastís Xocolata')
+        La recepta ja existeix
+        >>> Receptari.afegeix_recepta('Gofres')
         '''
+
         if self._receptes.has_key(n):
             print "La recepta ja existeix"
         else:
             self._receptes[n] = Recepta(n)
 
+
     def afegeix_producte(self, nomp):
         '''
         Afegeix un nou Producte de nom nomp. Si el producte ja existeix es queixa.
+
+        >>> Receptari = Receptari()
+        >>> Receptari.afegeix_producte('Farina')
+        >>> Receptari.afegeix_producte('Ous')
+        >>> Receptari.afegeix_producte('Farina')
+        El producte ja existeix
         '''
 
         if nomp in self._productes.keys():
@@ -42,28 +57,43 @@ class Receptari(object):
         else:
             self._productes[nomp] = Recepta(nomp)
 
+
     def afegeix_ingredient_recepta(self,nomr,nomp,q):
         '''
         Afegeix q grams de l’ingredient de nom nomp a la recepta de nom nomr. Si la recepta o
         l’ingredient no existeix es queixa.
+
+        >>> Receptari = Receptari()
+        >>> Receptari.afegeix_producte('Farina')
+        >>> Receptari.afegeix_ingredient_recepta('Gofre', 'Farina', 500)
+        La recepta < Gofre > no existeix
+        >>> Receptari.afegeix_recepta('Gofre')
+        >>> Receptari.afegeix_producte('Ous')
+        >>> Receptari.afegeix_producte('Farina')
+        El producte ja existeix
+        >>> Receptari.afegeix_ingredient_recepta('Gofre', 'Farina', 500)
+        >>> Receptari.afegeix_ingredient_recepta('Gofre', 'Ous', 500)
+        >>> Receptari.afegeix_ingredient_recepta('Gofre', 'Llet', 500)
+        El producte < Llet > no existeix
         '''
         if (nomr not in self._receptes.keys()):
-            print "La recepta no existeix"
+            print ("La recepta < %s > no existeix")%(nomr)
         else:
             if (nomp not in self._productes.keys()):
-                print "El producte no existeix"
+                print ("El producte < %s > no existeix")%(nomp)
             else:
                 self._receptes[nomr].afegeix_ingredient(nomp, q)
 
-        '''
-        if afegeix_ingredient(nomp) not in self._receptes:
-            print "L'ingredient no existeix"
-        else:
-        '''
-
+    
     def receptes_ingredient(self,nomp):
         '''
         Retorna la llista de noms de receptes en que participa l’ingredient de nom nomp.
+
+        >>> Receptari = Receptari()
+        >>> Receptari.afegeix_producte('Farina')
+        >>> Receptari.afegeix_producte('Ous')
+        >>> Receptari.afegeix_producte('Llet')
+        >>> Receptari.afegeix_producte('Sucre')
         '''
         l = []
         for recepta in self._receptes.values():
@@ -71,11 +101,13 @@ class Receptari(object):
                 l.append(recepta.nom)
         return l
 
+
     def receptes(self):
         '''
         Retorna la llista de noms de receptes del receptari.
         '''
         return self._receptes.keys()
+
 
     def ingredients(self):
         '''
@@ -83,10 +115,10 @@ class Receptari(object):
         '''
         return self._productes.keys()
 
+
     def recepta(self,nomr):
         '''
         Retorna una llista de parells (nomp, q) cadascun dels quals representa un ingredient de la
         recepta nomr. Cada parell agrega el nom del producte nomr i el pes necessari en grams q.
         '''
-
         return self._receptes[nomr]._ingredients
