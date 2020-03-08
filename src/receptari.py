40ebd139b6a208221bba19cb7e6c6dd3ad7ae843
Danielo24-bits <56434987+Danielo24-bits@users.noreply.github.com>
2020-03-08 16:59:58 +0100

    Add files via upload

--- src/receptari.py
+++ src/receptari.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 '''
 Module *receptari*
 ==================
 
-El mòdul receptari.py conté la classe Receptari. La classe ha de tenir els següents atributs:
+El mòdul receptari.py conté la classe *Receptari*, la qual està fonamentalment 
+formada per múltiples objectes de tipus *Recepta*. S'ha decidit representar als 
+objectes de la classe mitjançant dos atributs principals: 
+
 receptes [dict, Privat]
-    És un diccionari en el que la clau correspon al nom de totes les _receptes
+    És un diccionari en el que la clau correspon al nom de totes les receptes
     del receptari i el valor correspon a l’objecte Recepta corresponent.
 
 productes [dict, Privat]
     És un diccionari en el que la clau correspon al nom de tots els productes
-    que intervenen en el receptari i el valor correspon a l’objecte Producte corresponent.
+    que intervenen en el receptari i el valor correspon a l’objecte Producte 
+    corresponent.
+
+La classe *Receptari* és de tipus mutable, ja què conté els mètodes modificadors
+*afegeix_recepta*, *afegeix_producte* i *afegeix_ingredient_recepta*. La resta
+de mètodes no modificadors de la classe són: *receptes_ingredient*, *receptes*,
+ *ingredients* i *recepta*. En total, 7 mètodes específics dels objectes tipus
+*Receptari*, explicats a continuació:    
+
 '''
 
 from recepta import*
@@ -29,13 +39,14 @@ class Receptari(object):
 
     def afegeix_recepta(self,n):
         '''
-        Modificador. Afegeix una recepta de nom n al receptari. Si existia una recepta del mateix
-        nom es queixa.
+        Modificador. Afegeix una recepta de nom n al receptari. Si existia una 
+        recepta del mateix nom es queixa.
         La recepta, inicialment no té ingredients.
 
         :param n: Nom de la Recepta
         :type n: str
         :returns: Res
+        :rtype:
 
         >>> Receptari = Receptari()
         >>> Receptari.afegeix_recepta('Pastis Xocolata')
@@ -52,7 +63,8 @@ class Receptari(object):
 
     def afegeix_producte(self, nomp):
         '''
-        Afegeix un nou Producte de nom nomp. Si el producte ja existeix es queixa.
+        Afegeix un nou Producte de nom nomp. Si el producte ja existeix es 
+        queixa.
 
         >>> Receptari = Receptari()
         >>> Receptari.afegeix_producte('Farina')
@@ -69,8 +81,8 @@ class Receptari(object):
 
     def afegeix_ingredient_recepta(self,nomr,nomp,q):
         '''
-        Afegeix q grams de l’ingredient de nom nomp a la recepta de nom nomr. Si la recepta o
-        l’ingredient no existeix es queixa.
+        Afegeix q grams de l’ingredient de nom nomp a la recepta de nom nomr.
+        Si la recepta o l’ingredient no existeix es queixa.
 
         >>> Receptari = Receptari()
         >>> Receptari.afegeix_producte('Farina')
@@ -96,7 +108,8 @@ class Receptari(object):
     
     def receptes_ingredient(self,nomp):
         '''
-        Retorna la llista de noms de receptes en que participa l’ingredient de nom nomp.
+        Retorna la llista de noms de receptes en que participa l’ingredient de 
+        nom nomp.
 
         >>> Receptari = Receptari()
         >>> Receptari.afegeix_recepta('Gofre')
@@ -121,6 +134,7 @@ class Receptari(object):
         ['Gofre']
         >>> Receptari.receptes_ingredient('Ous')
         []
+
         '''
         l = []
         for recepta in self._receptes.values():
@@ -166,8 +180,9 @@ class Receptari(object):
 
     def recepta(self,nomr):
         '''
-        Retorna una llista de parells (nomp, q) cadascun dels quals representa un ingredient de la
-        recepta nomr. Cada parell agrega el nom del producte nomr i el pes necessari en grams q.
+        Retorna una llista de parells (nomp, q) cadascun dels quals representa 
+        un ingredient de la recepta nomr. Cada parell agrega el nom del producte
+        nomr i el pes necessari en grams q.
 
         >>> Receptari = Receptari()
         >>> Receptari.afegeix_recepta('Gofre')
